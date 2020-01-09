9561c67a5e9d14137e7cb6c18027d17531c5d887
ron34353435 <ron34353435@gmail.com>
2020-01-09 21:48:22 +0200

    #templates

--- vshop/urls.py
+++ vshop/urls.py
@@ -1,8 +1,9 @@
 # pages/urls.py
 from django.urls import path
 
-from .views import homePageView
+from .views import HomePageView, AboutView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about')
 ]
