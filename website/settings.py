7f9a6c52122f460e883ceced4660eb90f1fed12d
ron34353435 <ron34353435@gmail.com>
2020-01-10 01:04:52 +0200

    sign in & sign up

--- website/settings.py
+++ website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'vshop.apps.VshopConfig',
+#    'vshop.apps.VshopConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
